e62c298a91dfc338e89ac5ab44dc35181f8c5ad6
Leeroy Ding <i@leeroy.im>
2016-01-04 12:45:12 +0000

    Update seeding behaviour

--- Todo/AppDelegate+Data.swift
+++ Todo/AppDelegate+Data.swift
@@ -13,7 +13,12 @@ extension AppDelegate {
   func seedData() {
     let realm = try! Realm()
     guard realm.isEmpty else { return }
-    let todos = [Todo(text: "Yo", done: false)]
+    let todos = [
+      Todo(text: "First item", done: false),
+      Todo(text: "Second item", done: true),
+      Todo(text: "Third item", done: false),
+      Todo(text: "Fourth item is a very very long item that will break into the next line", done: false)
+    ]
     try! realm.write {
       realm.add(todos)
     }
--- Todo/AppDelegate.swift
+++ Todo/AppDelegate.swift
@@ -14,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    #if DEBUG
     seedData()
+    #endif
     window?.rootViewController = rootViewController()
     return true
   }
